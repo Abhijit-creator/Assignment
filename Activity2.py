7213832879c1ebda0f528bc49f1b09d6eab26c74
Abhijit-creator <70905251+Abhijit-creator@users.noreply.github.com>
2020-09-19 22:52:43 +0530

    Add files via upload

---
+++ Activity2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+for i in range(5):
+    for j in range(i+1):
+        print("*",end = " ")
+    print() 
+for i in range(4):
+    for j in range(4-i):
+        print("*",end = " ")
+    print()    
+
+
+# In[ ]:
+
+
+a= input(" ")
+a[::-1]
+
+
+# In[ ]:
+
+
+
+
